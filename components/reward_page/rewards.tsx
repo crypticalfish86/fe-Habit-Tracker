7f44da9826990150d29a38e603efe26337a3e5aa
crypticalfish86 <jasonweerawardena@gmail.com>
2023-02-28 11:03:56 +0000

    formik update

--- components/reward_page/rewards.tsx
+++ components/reward_page/rewards.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, TextInput } from "react-native";
 import { useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
 import { RewardCard } from "./rewardcard";
 import uuid from "react-uuid";
 import { useRoute } from "@react-navigation/native";
+import { RewardForm } from "./Post_Form";
 
 // user_id hardcoded in App.tsx
 
@@ -38,6 +39,27 @@ export const Rewards = () => {
     fetchData();
   }, [user_id]);
 
+
+  const postReward = async (event : any, Body : Object) =>{
+    event.PreventDefault()
+    const requestBody = 
+    {
+      method: 'POST',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify(Body)
+    }
+    try {
+      const response = await fetch(
+        `https://final-api.onrender.com/users/${user_id}/rewards`
+      );
+      const data = await response.json();
+      setUserRewards(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <View>
       {userRewards.map((reward: UserRewards, index: number) => (
@@ -49,6 +71,7 @@ export const Rewards = () => {
           user_id={reward.user_id}
         />
       ))}
+      < RewardForm />
     </View>
   );
 };
